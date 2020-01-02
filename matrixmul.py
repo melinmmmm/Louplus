a248236d86f0b2c956163f2bcf8e7f162ba91550
melin <1336510291@qq.com>
2020-01-02 10:46:48 +0800

    add matrixmul script

---
+++ matrixmul.py
@@ -0,0 +1,21 @@
+#!/bin/bash/env python3
+n = int(input('Enter the demission of mitrix:'))
+print('Enter values of the mitrix A')
+a = []
+for i in range(n):
+    a.append([int(x) for x in input().split()])
+print('Enter values of mitrix B')
+b = []
+for i in range(n):
+    b.append([int(x) for x in input().split()])
+c = []
+for i in range(n):
+    c.append([a[i][j] * b[i][j] for j in range(n)])
+print('After matrix multipliaction')
+print('-' * 7 * n)
+for x in c:
+    #print(x)
+    for y in x:
+        print(str(y).rjust(5), end = ' ')
+    print()
+print('-' * 7 * n)
